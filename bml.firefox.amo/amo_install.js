58f88d2654d0fe14bcbd79b03aa8138ddf90d13b
pctao <pctao@pixnet.tw>
2009-09-16 03:19:05 +0800

    修改 typo

--- bml.firefox.amo/amo_install.js
+++ bml.firefox.amo/amo_install.js
@@ -1,7 +1,7 @@
 $("p.install-button:has(a[iseulapagelink!=true])").prepend($('<input type="checkbox" class="install" checked="checked">安裝</input>'));
 $("<button>安裝</button>").click(function(i){
 var l = {};
-var amo = "<link:url>https://addons.mozilla.org/</link:url>";
+var amo = "https://addons.mozilla.org/";
 $("div.item:has(input.install:checked)").each(function(i) {
   var a = $(this).find("a.button.positive");
   l[a.attr('addonname')] = {
